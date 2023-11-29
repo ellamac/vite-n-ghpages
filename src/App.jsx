362b94b96346b1d5cd579d56178dce92d4dc475d
Ella Mäkelä <ella@makela.co>
2023-11-29 13:45:07 +0200

    made it fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-
+  const [howManySmileys, setHowManySmileys] = useState(0);
+  const smiley = '😃';
   return (
     <main>
      <h1>this is my vite + react app deployed to github pages 😃</h1>
+     <button onClick={() => setHowManySmileys(prev => prev + 1)}>😃<sup>😃</sup></button>
+     <h2>{smiley.repeat(Math.pow(howManySmileys,howManySmileys))}</h2>
     </main>
   )
 }
